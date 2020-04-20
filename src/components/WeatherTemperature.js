34f576de02eb1d14420805ec8ca971065da4962e
Pablo Betancor Lugo <p.betancor@gmail.com>
2020-04-20 17:15:21 +0200

    Add all icons in constants

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,15 +1,20 @@
 import React from "react";
 import WeatherIcons from "react-weathericons";
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from "../constants/weather";
 
 const icons = {
-  sunny: "day-sunny",
-  fog: "day-fog",
+  [SUN]: "day-sunny",
+  [CLOUD]: "cloud",
+  [CLOUDY]: "cloudy",
+  [RAIN]: "rain",
+  [SNOW]: "snow",
+  [WINDY]: "windy",
 };
 
 const getWeatherIcon = (weatherState) => {
   const icon = icons[weatherState];
   if (icon) {
-    return <WeatherIcons name={weatherState} size="2x" />;
+    return <WeatherIcons name={icon} size="2x" />;
   } else {
     return <WeatherIcons name={"day-sunny"} size="2x" />;
   }
